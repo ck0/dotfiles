e8508a13e6ac8920e966ddaa88ff3feae83476fc
chris <chris@c24t.org>
2012-03-28 11:21:17 -0700

    More setup script fixes
    Mac OS doesn't ships with a busted readlink, added a function to work
    around it. Cross-platform!

--- setup.zsh
+++ setup.zsh
@@ -1,24 +1,29 @@
 #! /usr/bin/env zsh
 
-dotdir=$(readlink -f $(pwd))
+# because OSX's `readlink` is all retarded
+function readlinkf () {(cd -P $1 && echo $(pwd))}
+
+dotdir=$(readlinkf $(pwd))
 backupdir=dotfiles.$(date +%s).backup
 
-(
-cd $HOME
-mkdir $backupdir
-mv .aliases $backupdir
-ln -svf $dotdir/dotaliases .aliases
-mv .vimrc $backupdir
-ln -svf $dotdir/dotvimrc .vimrc
-mv .inputrc $backupdir
-ln -svf $dotdir/dotinputrc .inputrc
-mv .zshrc $backupdir
-ln -svf $dotdir/dotzshrc .zshrc
-mv .pythonrc $backupdir
-ln -svf $dotdir/dotpythonrc .pythonrc
-mv .rubyrc $backupdir
-ln -svf $dotdir/dotrubyrc .rubyrc
+function dotrelink () {
+  if [[ -a $2 ]]; then
+    mv -v $2 $backupdir;
+  fi
+  ln -svf $dotdir/$1 $2
+}
 
-source .zshrc
+(
+  cd $HOME
+  mkdir $backupdir
+
+  dotrelink dotaliases .aliases
+  dotrelink dotvimrc .vimrc
+  dotrelink dotinputrc .inputrc
+  dotrelink dotzshrc .zshrc
+  dotrelink dotpythonrc .pythonrc
+  dotrelink dotrubyrc .rubyrc
+
+  source .zshrc
 )
 
