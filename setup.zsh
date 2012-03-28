6c9b271591540511405b1134d8b7bd8843eb3761
chris <chris@c24t.org>
2012-03-28 10:41:04 -0700

    Fixes to setup script

--- setup.zsh
+++ setup.zsh
@@ -1,14 +1,24 @@
-#! usr/bin/env zsh
+#! /usr/bin/env zsh
 
 dotdir=$(readlink -f $(pwd))
+backupdir=dotfiles.$(date +%s).backup
 
 (
 cd $HOME
+mkdir $backupdir
+mv .aliases $backupdir
 ln -svf $dotdir/dotaliases .aliases
+mv .vimrc $backupdir
 ln -svf $dotdir/dotvimrc .vimrc
+mv .inputrc $backupdir
 ln -svf $dotdir/dotinputrc .inputrc
+mv .zshrc $backupdir
 ln -svf $dotdir/dotzshrc .zshrc
+mv .pythonrc $backupdir
 ln -svf $dotdir/dotpythonrc .pythonrc
+mv .rubyrc $backupdir
 ln -svf $dotdir/dotrubyrc .rubyrc
+
+source .zshrc
 )
 
